2ef7957d83721ddca97fb09d6921b16eaa879eba
Moh. Rofikur Rutab <rofikur.rr@gmail.com>
2023-03-16 10:59:24 +0700

    update

---
+++ resources/views/admin/SpekAlat/spekalat_edit.blade.php
@@ -0,0 +1,55 @@
+@extends('admin.admin_master')
+@section('admin')
+ <main id="main" class="main">
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Edit Spesifikasi Alat</h5>
+
+              <!-- General Form Elements -->
+              <form>
+              <div class="row mb-3">
+                  <label for="inputText" class="col-sm-3 col-form-label">Kode Alat</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-3 col-form-label">Nama Alat</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-3 col-form-label">Brand</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div><div class="row mb-3">
+                  <label for="inputText" class="col-sm-3 col-form-label">Model</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div><div class="row mb-3">
+                  <label for="inputText" class="col-sm-3 col-form-label">Nomer Seri</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label class="col-sm-3 col-form-label"></label>
+                  <div class="col-sm-9">
+                    <button type="submit" class="btn btn-primary" style="margin-left: 247px;">Simpan</button>
+                  </div>
+                </div>
+
+              </form><!-- End General Form Elements -->
+        </div>
+      </div>
+    </section>
+</main>
+ @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlatController;
-
+use App\Http\Controllers\spekalatController;
+use App\Http\Controllers\detailalatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,5 +27,19 @@ Route::prefix('alat')->group(function(){
     // Route::post('/store', [FilmController::class,'FilmStore'])->name('film.store');
     Route::get('/edit', [AlatController::class,'edit'])->name('alat.edit');
     // Route::post('/update/{id}', [FilmController::class,'FilmUpdate'])->name('Film.update');
-   
+});
+Route::prefix('spekalat')->group(function(){
+    Route::get('/index', [spekalatController::class,'index'])->name('spekalat.view');
+    Route::get('/add', [spekalatController::class,'create'])->name('spekalat.add');
+    // Route::post('/store', [FilmController::class,'FilmStore'])->name('film.store');
+    Route::get('/edit', [spekalatController::class,'edit'])->name('spekalat.edit');
+    // Route::post('/update/{id}', [FilmController::class,'FilmUpdate'])->name('Film.update');
+});
+Route::prefix('detailalat')->group(function(){
+    Route::get('/index', [detailalatController::class,'index'])->name('detailalat.view');
+    Route::get('/indexdetail', [detailalatController::class,'indexdetail'])->name('detailalat.detail');
+    Route::get('/add', [detailalatController::class,'create'])->name('detailalat.add');
+    // Route::post('/store', [FilmController::class,'FilmStore'])->name('film.store');
+    Route::get('/edit', [detailalatController::class,'edit'])->name('detailalat.edit');
+    // Route::post('/update/{id}', [FilmController::class,'FilmUpdate'])->name('Film.update');
 });
